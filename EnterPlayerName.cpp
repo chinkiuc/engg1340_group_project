906bd5330fcfe17c44a46b7e5be11b9e41055889
chinkiuc <47135186+chinkiuc@users.noreply.github.com>
2020-04-29 04:30:58 +0800

    Delete EnterPlayerName.cpp

--- EnterPlayerName.cpp
+++
@@ -1,7 +0,0 @@
-void EnterPlayerName(){
-	
-	cout<<"\n\n\n\n     Hi, Player. \n\n     Welcome to <gamename> ! \n\n   ========================================\n\n     What's your name: ";
-	cin >> playername;
-	system("CLS");
-	
-}
