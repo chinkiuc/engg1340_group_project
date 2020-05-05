88ff192f8d4d4646ae77a0efc97bef563ede2163
chinkiuc <47135186+chinkiuc@users.noreply.github.com>
2020-05-05 22:48:22 +0800

    Update Begining.cpp

--- Begining.cpp
+++ Begining.cpp
@@ -1,3 +1,6 @@
+#include<iostream>
+
+
 int Begining(){
 	cout <<"\n\n\n\n"<<"      ------------  ENGG1340 Course Project  ------------ \n\n\n\n\n         Developed by:\n";
 	cout <<"\n            CHOI CHIN KIU      (3035557351)\n\n            DEV CHORADIA MANOJ (3035666669)\n\n\n\n\n\n         Press <ENTER> key to continue...... \n\n\n\n";
