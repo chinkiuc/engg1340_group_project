7b086ddda77c02020b4b57a05d4a24e05cd09cff
chinkiuc <47135186+chinkiuc@users.noreply.github.com>
2020-05-09 00:47:50 +0800

    Update makefile.h

--- makefile.h
+++ makefile.h
@@ -1,12 +1,26 @@
-$make main_main
+Beginning.o: Beginning.cpp
+	g++ -c Beginning.cpp
 
-g++ -c Beginning.cpp
-g++ -c ChangePlayerName.cpp
-g++ -c EnterPlayerName.cpp
-g++ -c gameover.cpp
-g++ -c GameRule.cpp
-g++ -c ScoreBoard.cpp
-g++ -c swap.cpp
-g++ -c main.cpp
+ChangePlayerName.o: ChangePlayerName.cpp
+	g++ -c ChangePlayerName.cpp
 
-g++ -o main Beginning.o ChangePlayerName.o EnterPlayerName.o gameover.o GameRule.o ScoreBoard.o swap.o main.o
+EnterPlayerName.o: EnterPlayerName.cpp	
+	g++ -c EnterPlayerName.cpp
+	
+gameover.o: gameover.cpp
+	g++ -c gameover.cpp
+	
+GameRule.o: GameRule.cpp	
+	g++ -c GameRule.cpp
+	
+ScoreBoard.o: ScoreBoard.cpp
+	g++ -c ScoreBoard.cpp
+	
+swap.o: swap.cpp
+	g++ -c swap.cpp
+
+main.o: main.cpp	
+	g++ -c main.cpp
+
+main: Beginning.o ChangePlayerName.o EnterPlayerName.o gameover.o GameRule.o ScoreBoard.o swap.o main.o
+	g++ Beginning.o ChangePlayerName.o EnterPlayerName.o gameover.o GameRule.o ScoreBoard.o swap.o main.o -o main
