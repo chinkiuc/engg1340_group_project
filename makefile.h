7609429732248b5d0c3c37b551a181e6343f6dc5
chinkiuc <47135186+chinkiuc@users.noreply.github.com>
2020-05-09 00:24:09 +0800

    Add files via upload

---
+++ makefile.h
@@ -0,0 +1,10 @@
+g++ -c Beginning.cpp
+g++ -c ChangePlayerName.cpp
+g++ -c EnterPlayerName.cpp
+g++ -c gameover.cpp
+g++ -c GameRule.cpp
+g++ -c ScoreBoard.cpp
+g++ -c swap.cpp
+g++ -c main.cpp
+
+g++ -o main Beginning.o ChangePlayerName.o EnterPlayerName.o gameover.o GameRule.o ScoreBoard.o swap.o main.o
